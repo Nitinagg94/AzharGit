a92833bb8903b63c2b41d3f784768689754afa7b
user <user@user-PC>
2019-09-22 16:19:56 +0530

    First Test

---
+++ GitTest/src/GitHub/GitClass.java
@@ -0,0 +1,11 @@
+package GitHub;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello This is GitTesting!");
+
+	}
+
+}
